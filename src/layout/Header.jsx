2b6921dc9e1c686a5d28a5b358d56608a6b0c3e2
Mikhail Dronov <mihey3000@yandex.ru>
2021-03-01 00:20:36 +0500

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <nav className="light-blue darken-3">
+      <div className="container nav-wrapper">
+        <a href="!#" className="brand-logo">
+          React Movies
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="!#">Repo</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export { Header };
